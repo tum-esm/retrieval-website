58df2047fcc8c4fb9736f3c7867b06ccf06ca289
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-08-24 16:12:45 +0200

    Set up tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "retrieval-plots-v2",
-  },
-  plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-mdx",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
+    siteMetadata: {
+        siteUrl: 'https://www.yourdomain.tld',
+        title: 'retrieval-plots-v2',
     },
-  ],
+    plugins: [
+        'gatsby-plugin-postcss',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-mdx',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+    ],
 };
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <main>
+            <title>ESM Plots</title>
+            <h1 className='p-2 m-4 bg-green-200 rounded text-green-800 font-bold text-center'>
+                ESM Plots
+            </h1>
+        </main>
+    );
+};
+
+export default Page;

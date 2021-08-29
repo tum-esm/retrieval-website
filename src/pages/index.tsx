deaa0647a9daf31b2dc63ab0791edacd36522ad3
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-08-29 20:15:56 +0200

    Implement campaign list on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import types from 'types';
+import { ArrowCircleRightIcon } from '@heroicons/react/solid';
+import { tail } from 'lodash';
 
 const Page = () => {
+    const query = useStaticQuery(graphql`
+        query {
+            allStrapiPlotMeta {
+                nodes {
+                    campaignId
+                }
+            }
+        }
+    `);
+    const metas = query.allStrapiPlotMeta.nodes;
+
+    const headingClasses = 'max-w-xs text-lg leading-tight text-center rounded';
     return (
-        <main>
-            <title>ESM Plots</title>
-            <h1 className='p-2 m-4 bg-green-200 rounded text-green-800 font-bold text-center'>
-                ESM Plots
+        <main className='w-screen min-h-screen px-4 py-8 bg-gray-100 flex-col-center'>
+            <h2
+                className={` text-lg mb-2 text-green-700 font-regular ${headingClasses}`}
+            >
+                TUM - Assistant Professorship of Environmental Sensing and
+                Modeling
+            </h2>
+            <h1
+                className={`mb-4 text-3xl text-green-900 font-semibold ${headingClasses}`}
+            >
+                Retrieval Plots
             </h1>
+            <div className='max-w-sm w-[24rem] flex-row-center'>
+                {metas.map((m: types.plotMeta) => (
+                    <Link to={`/${m.campaignId}`} className='w-full'>
+                        <div className='w-full p-2 pl-3 bg-white rounded shadow flex-row-center'>
+                            <div className='text-base font-semibold text-gray-600'>
+                                {m.campaignId[0].toUpperCase()}
+                                {tail(m.campaignId)}
+                            </div>
+                            <div className='flex-grow' />
+                            <div className='w-8 h-8 p-1.5 text-gray-400 flex-shrink-0'>
+                                <ArrowCircleRightIcon />
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </main>
     );
 };

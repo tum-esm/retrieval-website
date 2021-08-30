177020c2df35f01339d8bb2f45468034dd89b1da
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-08-30 14:09:16 +0200

    Improve index page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,30 +16,32 @@ const Page = () => {
     `);
     const metas = query.allStrapiPlotMeta.nodes;
 
-    const headingClasses = 'max-w-xs text-lg leading-tight text-center rounded';
+    const headingClasses = 'text-lg leading-tight text-center rounded';
     return (
         <main className='w-screen min-h-screen px-4 py-8 bg-gray-100 flex-col-center'>
-            <h2
-                className={` text-lg mb-2 text-green-700 font-regular ${headingClasses}`}
-            >
-                TUM - Assistant Professorship of Environmental Sensing and
-                Modeling
-            </h2>
-            <h1
-                className={`mb-4 text-3xl text-green-900 font-semibold ${headingClasses}`}
-            >
-                Retrieval Plots
-            </h1>
-            <div className='max-w-sm w-[24rem] flex-row-center'>
+            <div className='max-w-md flex-col-center'>
+                <h2
+                    className={`max-w-xs text-lg mb-2 text-green-700 font-regular ${headingClasses}`}
+                >
+                    TUM - Assistant Professorship of Environmental Sensing and
+                    Modeling
+                </h2>
+                <h1
+                    className={`mb-4 text-2xl text-green-900 font-semibold ${headingClasses}`}
+                >
+                    Plots of the EM27 Retrieval Output
+                </h1>
+            </div>
+            <div className='max-w-md w-[28rem] flex-row-center'>
                 {metas.map((m: types.plotMeta) => (
                     <Link to={`/${m.campaignId}`} className='w-full'>
-                        <div className='w-full p-2 pl-3 bg-white rounded shadow flex-row-center'>
+                        <div className='w-full p-2 pl-3 bg-white rounded-md shadow flex-row-center'>
                             <div className='text-base font-semibold text-gray-600'>
                                 {m.campaignId[0].toUpperCase()}
                                 {tail(m.campaignId)}
                             </div>
                             <div className='flex-grow' />
-                            <div className='w-8 h-8 p-1.5 text-gray-400 flex-shrink-0'>
+                            <div className='flex-shrink-0 w-5 h-5 text-gray-400'>
                                 <ArrowCircleRightIcon />
                             </div>
                         </div>

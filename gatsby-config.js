2abf6992bcd87fcb8edd18fbc0e5cc0c473cb15a
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-14 18:46:06 +0100

    Nuke everything and start fresh v4 codebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,19 @@
-require('dotenv').config({ path: `.env` });
-
 module.exports = {
     siteMetadata: {
-        siteUrl: 'https://retrieval-plots.dostuffthatmatters.dev',
-        title: 'retrieval-plots-v2',
+        siteUrl: 'https://www.yourdomain.tld',
+        title: 'retrieval-website-v2',
     },
     plugins: [
         'gatsby-plugin-postcss',
         'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sitemap',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/icon.png',
+            },
+        },
         'gatsby-plugin-mdx',
-        'gatsby-plugin-root-import',
         {
             resolve: 'gatsby-source-filesystem',
             options: {
@@ -17,25 +21,6 @@ module.exports = {
                 path: './src/pages/',
             },
             __key: 'pages',
-        },
-        {
-            resolve: 'gatsby-source-strapi',
-            options: {
-                apiURL: process.env.API_URL || 'http://localhost:1337',
-                collectionTypes: ['plot-meta'],
-                queryLimit: 1000,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: 'EM27 Retrieval Plots',
-                short_name: 'EM27 Retrieval Plots',
-                start_url: '/',
-                background_color: '#ffffff',
-                theme_color: '#f60948',
-                icon: 'src/assets/favicon.ico',
-            },
         },
     ],
 };

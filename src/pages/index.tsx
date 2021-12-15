7a8b297aa26721a686855849acec36775890e807
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-15 19:42:53 +0100

    Add tailwind, style index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import React from 'react';
 import types from '../types';
+import { Helmet } from 'react-helmet';
+import icons from '../assets/icons';
+import { tail } from 'lodash';
 
 export default function Page() {
     const data = useStaticQuery(graphql`
@@ -13,19 +16,64 @@ export default function Page() {
             }
         }
     `);
+    const headingClasses = 'text-lg leading-tight text-center rounded';
 
     return (
-        <main>
-            <h1>EM27 Retrieval Plots</h1>
-            <ul>
-                {data.allCampaign.nodes.map((c: types.Campaign) => (
-                    <li>
-                        <Link to={`/${c.identifier}/${c.displayDate}`}>
-                            {c.identifier.toUpperCase()}
+        <>
+            <Helmet title='EM27 Retrieval' defer={false} />
+            <main className='w-screen min-h-screen px-4 py-16 bg-gray-100 flex-col-center'>
+                <div className='absolute bottom-0 left-0 z-0 w-full flex-row-center'>
+                    <div className='py-1 text-sm text-gray-400 font-weight-500'>
+                        {process.env.GATSBY_COMMIT_SHA
+                            ? `version ${process.env.GATSBY_COMMIT_SHA}`
+                            : 'development'}
+                    </div>
+                </div>
+                <div className='max-w-full flex-col-center'>
+                    <h2
+                        className={`max-w-xs text-lg mb-2 text-green-700 font-regular ${headingClasses}`}
+                    >
+                        TUM - Assistant Professorship of Environmental Sensing
+                        and Modeling
+                    </h2>
+                    <h1
+                        className={`mb-4 text-2xl text-green-900 font-semibold ${headingClasses}`}
+                    >
+                        Plots of the EM27 Retrieval Output
+                    </h1>
+                </div>
+                <div className='max-w-full w-[28rem] flex-row-center'>
+                    {data.allCampaign.nodes.map((c: types.Campaign) => (
+                        <Link
+                            to={`/${c.identifier}/${c.displayDate}`}
+                            className='w-full'
+                        >
+                            <div className='w-full p-2 pl-3 bg-white rounded-md shadow flex-row-center'>
+                                <div className='text-base font-semibold text-gray-600'>
+                                    {c.identifier[0].toUpperCase()}
+                                    {tail(c.identifier)}
+                                </div>
+                                <div className='flex-grow' />
+                                <div className='flex-shrink-0 w-5 h-5 text-gray-400 icon-campaign-list'>
+                                    {icons.arrowThickRightCircle}
+                                </div>
+                            </div>
                         </Link>
-                    </li>
-                ))}
-            </ul>
-        </main>
+                    ))}
+                </div>
+                <div className='text-sm mt-16 max-w-full w-[28rem] flex-col-center'>
+                    <h3 className={`text-gray-600 font-semibold`}>
+                        Do you want to use this data in your research?
+                    </h3>
+                    <a
+                        className={` text-blue-500 underline font-regular`}
+                        href='https://atmosphere.ei.tum.de/de/contact/'
+                        target='_blank'
+                    >
+                        https://atmosphere.ei.tum.de/de/contact/
+                    </a>
+                </div>
+            </main>
+        </>
     );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,84 @@
+const colors = require('tailwindcss/colors');
+
+const customColors = {
+    // Primary
+    blue: {
+        50: '#DCEEFB',
+        100: '#B6E0FE',
+        200: '#84C5F4',
+        300: '#62B0E8',
+        400: '#4098D7',
+        500: '#2680C2',
+        600: '#186FAF',
+        700: '#0F609B',
+        800: '#0A558C',
+        900: '#003E6B',
+    },
+
+    // Neutrals
+    gray: colors.slate,
+    'gray-950': '#0B111F',
+
+    green: {
+        50: '#F0FCF9',
+        100: '#C6F7E9',
+        200: '#8EEDD1',
+        300: '#5FE3C0',
+        400: '#2DCCA7',
+        500: '#17B897',
+        600: '#079A82',
+        700: '#048271',
+        800: '#016457',
+        900: '#004440',
+    },
+
+    red: {
+        50: '#FFEEEE',
+        100: '#FACDCD',
+        200: '#F29B9B',
+        300: '#E66A6A',
+        400: '#D64545',
+        500: '#BA2525',
+        600: '#A61B1B',
+        700: '#911111',
+        800: '#780A0A',
+        900: '#610404',
+    },
+
+    yellow: {
+        50: '#FFFAEB',
+        100: '#FCEFC7',
+        200: '#F8E3A3',
+        300: '#F9DA8B',
+        400: '#F7D070',
+        500: '#E9B949',
+        600: '#C99A2E',
+        700: '#A27C1A',
+        800: '#7C5E10',
+        900: '#513C06',
+    },
+
+    orange: colors.orange,
+    teal: colors.teal,
+    sky: colors.sky,
+    indigo: colors.indigo,
+    purple: colors.purple,
+    pink: colors.pink,
+    rose: colors.rose,
+};
+
+module.exports = {
+    content: [
+        './src/pages/**/*.{js,jsx,ts,tsx}',
+        './src/templates/**/*.{js,jsx,ts,tsx}',
+        './src/components/**/*.{js,jsx,ts,tsx}',
+        './src/assets/**/*.{js,jsx,ts,tsx}',
+        './src/utils/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: customColors,
+            fill: customColors,
+        },
+    },
+};

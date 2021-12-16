783124ee4dd15ff122cbede9f58ebce42438255b
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-16 23:16:32 +0100

    Add scss stylesheet for toggling plot layers

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
+import './src/styles/plot-elements.scss';
 import './src/styles/tailwind.css';
--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
 
     plugins: [
         'gatsby-plugin-postcss',
+        `gatsby-plugin-sass`,
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-sitemap',
         {

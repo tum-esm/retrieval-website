bf99f3716557c35a3ec13d82f5213d09bee25d2b
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-05-29 22:40:58 +0000

    Add new upload client (7)
    extend config

--- upload-client/src/config.py
+++ upload-client/src/config.py
@@ -8,12 +8,22 @@ _PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(__file__, current_depth=2
 
 
 class Config(pydantic.BaseModel):
+    data: Config.DataConfig
     cms: Config.CMSConfig
+    networking: Config.NetworkingConfig
+
+    class DataConfig(pydantic.BaseModel):
+        sensors_to_consider: list[str]
+        src_dir: str
 
     class CMSConfig(pydantic.BaseModel):
         url: str
         identity: str
         password: str
+
+    class NetworkingConfig(pydantic.BaseModel):
+        thread_count: int
+        max_requests_per_seconds: float
 
     def load() -> Config:
         with open(os.path.join(_PROJECT_DIR, "config.json"), "r") as f:

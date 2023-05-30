0586eec33312911bcf66fa080a6d20fde5556128
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-05-30 00:40:27 +0000

    Add new upload client (19)
    write output to log file

--- upload-client/run.py
+++ upload-client/run.py
@@ -1,32 +1,46 @@
 from datetime import datetime
+import os
+import tum_esm_utils
 import src
 import rich.console
 import rich.progress
 import tum_esm_em27_metadata
 
-console = rich.console.Console()
-config = src.config.Config.load()
-location_data = tum_esm_em27_metadata.load_from_github(**config.location_data.dict())
+PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(__file__, current_depth=1)
+LOG_DIR = f"{PROJECT_DIR}/logs"
+if not os.path.isdir(LOG_DIR):
+    os.makedirs(LOG_DIR)
 
-for sensor_id in config.data.sensors_to_consider:
-    console.print(f"Processing data from sensor {sensor_id}", style="bold blue")
+with open(
+    f"{LOG_DIR}/run-{datetime.utcnow().isoformat(timespec='seconds')}.ansi", "a"
+) as f:
+    console = rich.console.Console(color_system="standard", file=f)
+    config = src.config.Config.load()
+    location_data = tum_esm_em27_metadata.load_from_github(
+        **config.location_data.dict()
+    )
 
-    sensor_data_loader = src.data.SensorDataLoader(sensor_id, location_data)
-    pocketbase = src.pocketbase.PocketBase()
+    for sensor_id in config.data.sensors_to_consider:
+        console.print(f"Processing data from sensor {sensor_id}", style="bold blue")
 
-    # only returns dates that have not been uploaded yet
-    # prints out the number of new and cached dates
-    new_dates = sensor_data_loader.get_new_dates()
+        sensor_data_loader = src.data.SensorDataLoader(sensor_id, location_data)
+        pocketbase = src.pocketbase.PocketBase()
 
-    with rich.progress.Progress(console=console, auto_refresh=False) as progress:
-        task = progress.add_task(f"Uploading data of {sensor_id}", total=len(new_dates))
-        for date in new_dates:
-            console.print(
-                f"  Uploading [blue]{date}[/blue] ({datetime.utcnow().isoformat(timespec='seconds')} UTC)",
-                style="bold bright_white",
-                highlight=False,
+        # only returns dates that have not been uploaded yet
+        # prints out the number of new and cached dates
+        new_dates = sensor_data_loader.get_new_dates(console)
+
+        with rich.progress.Progress(console=console, auto_refresh=False) as progress:
+            task = progress.add_task(
+                f"Uploading data of {sensor_id}", total=len(new_dates)
             )
-            records = sensor_data_loader.get_date_records(date, console)
-            pocketbase.upload_records(sensor_id, date, records, console)
-            progress.update(task, advance=1, refresh=True)
-            sensor_data_loader.add_date_to_cache_list(date)
+            for date in new_dates:
+                console.print(
+                    f"  Uploading [blue]{date}[/blue] ({datetime.utcnow().isoformat(timespec='seconds')} UTC)",
+                    style="bold bright_white",
+                    highlight=False,
+                )
+                records = sensor_data_loader.get_date_records(date, console)
+                pocketbase.upload_records(sensor_id, date, records, console)
+                progress.update(task, advance=1, refresh=True)
+                sensor_data_loader.add_date_to_cache_list(date)

27b2ed1d921f6ed43e5689c38e825344fb081f08
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-05-30 15:15:19 +0200

    Add new frontent (5)
    move data fetching to lib directory

--- frontend/app/lib/custom-types.ts
+++ frontend/app/lib/custom-types.ts
@@ -1,14 +1,16 @@
 import { z } from 'zod';
 
-export const heatmapItemSchema = z.object({
-    collectionId: z.string(),
-    collectionName: z.string(),
-    id: z.string(),
-    total: z.number(),
-    utc: z.string(),
-    created: z.string(),
-    updated: z.string(),
-    expand: z.object({}),
-});
+export const heatmapItemSchema = z
+    .object({
+        collectionId: z.string(),
+        collectionName: z.string(),
+        id: z.string(),
+        total: z.number(),
+        utc: z.string(),
+        created: z.string(),
+        updated: z.string(),
+        expand: z.object({}),
+    })
+    .pick({ utc: true, total: true });
 
 export type HeatmapItemType = z.infer<typeof heatmapItemSchema>;
---
+++ frontend/app/lib/fetch-data-heatmap.ts
@@ -0,0 +1,19 @@
+import { HeatmapItemType, heatmapItemSchema } from '@/app/lib/custom-types';
+import PocketBase from 'pocketbase';
+
+export async function fetchDataHeatmap(): Promise<HeatmapItemType[]> {
+    const pb = new PocketBase('https://esm-linode.dostuffthatmatters.dev');
+    await pb
+        .collection('users')
+        .authWithPassword(
+            process.env.NEXT_PUBLIC_POCKETBASE_IDENTITY || '',
+            process.env.NEXT_PUBLIC_POCKETBASE_PASSWORD || ''
+        );
+    if (!pb.authStore.isValid) {
+        throw new Error('Invalid auth data');
+    }
+    const results = await pb.collection('data_heatmap').getFullList({
+        sort: 'utc',
+    });
+    return results.map(item => heatmapItemSchema.parse(item));
+}
